44dfc8f14c99970e060e3c5829b12b2e6871a334
Fabrizio <fabgrel10@gmail.com>
2022-06-27 17:54:37 -0500

    🧐 Add relations and validation at model level

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  has_many :categories, dependent: :destroy
+  has_many :expenses, dependent: :destroy
+
+  validates :name, presence: true, format: { with: /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/,
+                                             message: 'Invalid name!',
+                                             maximum: 250 }
+  validates :email, presence: true, length: { maximum: 100 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
 end
