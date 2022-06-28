656ae4c58bce924b15166ad0a346a3cb7b2a105a
Fabrizio <fabgrel10@gmail.com>
2022-06-28 16:03:25 -0500

    🔧 Add additional configuration to Devise and setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    authenticated do
+      root to: 'categories#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+  root to: 'home#index', as: :unauthenticated_root
+    end
+
+    get '/logout', to: 'devise/sessions#destroy'
+  end
+
+  resources :categories, only: %i[index new create show] do
+    resources :expenses, only: %i[index new create show]
+  end
 end
